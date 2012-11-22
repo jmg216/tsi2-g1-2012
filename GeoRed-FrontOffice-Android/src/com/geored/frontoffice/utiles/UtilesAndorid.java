fe951866e2c802335918fe0a6f6ebc65c8ac127f
marcelobasan09@gmail.com <marcelobasan09@gmail.com@a4ebab49-0027-b9c8-d998-7a49add07733>
2012-11-22 22:46:10 +0000

    Pronto el tema de mostrar los sitios y la ubicacion del usuario, agregado el radio de proximidad seteable por el usuario (opcional sino se muestran todos). Si se clickea en los overlay opcion de ir a la vista del sitio.

--- GeoRed-FrontOffice-Android/src/com/geored/frontoffice/utiles/UtilesAndorid.java
+++ GeoRed-FrontOffice-Android/src/com/geored/frontoffice/utiles/UtilesAndorid.java
@@ -4,6 +4,8 @@ public class UtilesAndorid {
 
 	public static final String IP_PUERTO="10.0.2.2:8080";
 	
+	public static final double radioTierraKm = 6371;
+	
 	public static final String URL_WS_USUARIO = "http://" + IP_PUERTO +"/GeoRed-Business-EJB/UsuarioServiceImpl?wsdl";
 	public static final String URL_WS_SITIO   = "http://" + IP_PUERTO +"/GeoRed-Business-EJB/SitioServiceImpl?wsdl";
 	public static final String URL_WS_ADMIN   = "http://" + IP_PUERTO +"/GeoRed-Business-EJB/AdminServiceImpl?wsdl";
@@ -12,5 +14,34 @@ public class UtilesAndorid {
 	public static final String URL_WS_EVENTO  = "http://" + IP_PUERTO +"/GeoRed-Business-EJB/EventoServiceImpl?wsdl";
 	public static final String URL_WS_GLOBAL  = "http://" + IP_PUERTO +"/GeoRed-Business-EJB/GlobalServiceImpl?wsdl";
 	public static final String URL_WS_COMPRA  = "http://" + IP_PUERTO +"/GeoRed-Business-EJB/CompraServiceImpl?wsdl";
+
+	
+	public static double CalcularDistanciaCoordenadas(String strLat1, String strLon1, String strLat2, String strLon2)
+    {
+        double lat1 = Rad(Double.parseDouble(strLat1));
+        double lon1 = Rad(Double.parseDouble(strLon1));
+        double lat2 = Rad(Double.parseDouble(strLat2));
+        double lon2 = Rad(Double.parseDouble(strLon2));
+
+
+        // Calculo la diferencia de distancias en radianes
+        double dLat = lat2 - lat1;
+        double dLon = lon2 - lon1;
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                   Math.cos(lat1) * Math.cos(lat2) *
+                   Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        //Devuelve la distancia entre los dos puntos.
+        return (radioTierraKm * c);
+    }
+	
+	private static double Rad(double x)
+    {
+		return x * (Math.PI / 180);
+    }
+
 	
 }
